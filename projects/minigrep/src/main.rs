ca726937da833b5662d1b3a5e574b4d4733d70e7
MatthewWinnan <71087625+MatthewWinnan@users.noreply.github.com>
2025-07-10 21:53:53 +0200

    Just updating progress, switching devices

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,3 +1,20 @@
+use minigrep::{Config, run};
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    match run(config) {
+        Err(e) => {
+            println!("Application error: {e}");
+            process::exit(1);
+        }
+        Ok(()) => {
+            println!("Works");
+        }
+    }
 }
